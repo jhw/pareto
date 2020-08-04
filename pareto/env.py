1b8cebcd1f0fa6b396a95ee9436d38928c9c2d1f
Justin <justin.worrall@gmail.com>
2020-08-04 11:31:46 +0100

    simplified env

--- pareto/env.py
+++ pareto/env.py
@@ -10,17 +10,7 @@ from pareto.components.table import synth_table
 from pareto.components.timer import synth_timer
 from pareto.components.website import synth_website
 
-"""
-- `init_templates` is super- opinionated about layouts
-- bundles triggers, actions and related permissions/mappings under same nested templates so each can be indepedent / minimal parameter requiring required
-- also makes use of template.dashboard which may be an over- optimisation
-- but is not the only way to do it - could have triggers, actions and dashes in different stacks
-- really all actions should export arns, just in case
-- and triggers should be created with option of using parameter- based arns, in case you want to mess with the layout
-- then could maybe experiment with different layouts at env level
-"""
-
-def init_templates(config, templates, outputs):
+def init_templates(config):
     def template_name(config, groupkey):
         return "%s-%s-%s" % (config["globals"]["app"],
                              groupkey,
@@ -34,11 +24,13 @@ def init_templates(config, templates, outputs):
             component=fn(**kwargs)
             template.update(component)            
         return template
+    templates, outputs = {}, {}
     for groupkey, components in config["components"].items():
         template=init_template(config, groupkey, components)
         outputs.update({outputkey: groupkey
                         for outputkey, _ in template.outputs})
         templates[groupkey]=template.render()
+    return templates, outputs
 
 def init_master(config, templates, outputs):
     def nested_param(outputs, paramname):
@@ -51,19 +43,16 @@ def init_master(config, templates, outputs):
             stack["params"]={paramname: nested_param(outputs, paramname)
                              for paramname in template["Parameters"]}
         return stack
-    def init_master(config, templates):
-        master=Template()
-        for tempname, template in templates.items():
-            kwargs=init_stack(config, tempname, template, outputs)
-            stack=synth_stack(**kwargs)
-            master.update(stack)
-        return master.render()
-    templates["master"]=init_master(config, templates)
+    master=Template()
+    for tempname, template in templates.items():
+        kwargs=init_stack(config, tempname, template, outputs)
+        stack=synth_stack(**kwargs)
+        master.update(stack)
+    return master.render()
         
 def synth_env(config):
-    templates, outputs = {}, {}
-    init_templates(config, templates, outputs)
-    init_master(config, templates, outputs)
+    templates, outputs = init_templates(config)
+    templates["master"]=init_master(config, templates, outputs)    
     return templates
 
 if __name__=="__main__":
