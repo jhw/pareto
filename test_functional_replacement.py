3bf4e876d52371aa45022b41f12758ba31f1a80f
Justin <justin.worrall@gmail.com>
2020-08-06 10:27:57 +0100

    added decorator

--- test_functional_replacement.py
+++ test_functional_replacement.py
@@ -6,12 +6,19 @@ class Counter:
     def increment(self):
         self.counter+=1
 
-def matcher(match, counter=Counter()):
-    resp="hah" if 0==counter.counter % 2 else 'bah'
-    counter.increment()
-    return resp
+def increment(fn):
+    def wrapped(match, counter):
+        resp=fn(match, counter)
+        counter.increment()
+        return resp
+    return wrapped
+        
+@increment        
+def matcher(match, counter):
+    return "hah" if 0==counter.counter % 2 else 'bah'
 
 if __name__=="__main__":
     counter=Counter()
-    print (re.sub("XXX", matcher, 
+    print (re.sub("XXX",
+                  lambda m: matcher(m, counter),
                   "abasdasdad XXX asjdasdsd XXX asdasdd XXX ashdahjsadsa XXX asasasas"))
