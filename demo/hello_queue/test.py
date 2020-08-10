39dfa0ad913c787eea7713b8dd7d1c38fa4c5057
Justin <justin.worrall@gmail.com>
2020-08-10 07:59:39 +0100

    removed -action suffix from actions and added hello service

---
+++ demo/hello_queue/test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class IndexTest(unittest.TestCase):
+
+    def test_handler(self):
+        from hello_queue.index import handler
+        resp=handler({"hello": "world"}, None)
+        self.assertTrue("hello" in resp)
+
+if __name__=="__main__":
+    unittest.main()
