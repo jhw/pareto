68c0fd075f9478a7eee0b4c966ec54f347c91ea0
Justin <justin.worrall@gmail.com>
2020-08-16 20:59:33 +0100

    extended http code to allow headers to be set

--- pareto/scripts/ping_api.py
+++ pareto/scripts/ping_api.py
@@ -4,38 +4,63 @@ from pareto.scripts import *
 
 from pareto.helpers.text import hungarorise
 
-def validate_form_urlencoded(fn):
+def validate_get_payload(fn):
     def is_form_urlencoded(payload):
         return re.search("^(\\w+\\=\\w+\\&)*\\w+\\=\\w+$", payload)!=None
-    def wrapped(url, payload):
+    def wrapped(url, payload, token):
         if is_form_urlencoded(payload):                
-            return fn(url, payload)
+            return fn(url="%s?%s" % (url, payload),
+                      token=token)
         else:
             raise RuntimeError("GET payload is invalid")
     return wrapped
 
-@validate_form_urlencoded
-def http_get(url, payload):
-    return requests.get("%s?%s" % (url, payload))
+def init_get_headers(fn):
+    def wrapped(url, token):
+        headers={}
+        if token:
+            headers["Authorization"]="Bearer %s" % token
+        return fn(url=url,
+                  headers=headers)
+    return wrapped
 
-def validate_json(fn):
+@validate_get_payload
+@init_get_headers
+def http_get(url, headers):
+    return requests.get(url,
+                        headers=headers)
+
+def validate_post_payload(fn):
     def is_json(payload):
         try:
             json.loads(payload)
             return True
         except:
             return False
-    def wrapped(url, payload):
+    def wrapped(url, payload, token):
         if is_json(payload):                
-            return fn(url, payload)
+            return fn(url=url,
+                      payload=payload,
+                      token=token)
         else:
             raise RuntimeError("POST payload is invalid")
     return wrapped
 
-@validate_json
-def http_post(url, payload):
+def init_post_headers(fn):
+    def wrapped(url, payload, token):
+        headers={"Content-Type": "application/json"}
+        if token:
+            headers["Authorization"]="Bearer %s" % token
+        return fn(url=url,
+                  headers=headers,
+                  payload=payload)
+    return wrapped
+
+@validate_post_payload
+@init_post_headers
+def http_post(url, headers, payload):
     return requests.post(url,
-                         headers={"Content-Type": "application/json"},
+                         headers=headers,
                          data=payload)
     
 if __name__=="__main__":
@@ -77,7 +102,9 @@ if __name__=="__main__":
         url=outputs.lookup("%s-%s-url" % (api["name"],
                                           resource["name"]))
         httpfn=eval("http_%s" % resource["method"].lower())
-        resp=httpfn(url, args["payload"])
+        resp=httpfn(url=url,
+                    payload=args["payload"],
+                    token=None)
         for attr in ["status_code",
                      "headers",
                      "text"]:
