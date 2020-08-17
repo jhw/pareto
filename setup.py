f5bdbf930d410796f85d7a95eb43ef69582fb378
Justin <justin.worrall@gmail.com>
2020-08-17 07:43:06 +0100

    went for maximal setup.py option

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pareto",
-    version="1.0.2",
+    version="1.0.3",
     author="jhw",
     author_email="justin.worrall@gmail.com",
     description="OTP for serverless",
@@ -17,6 +17,19 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    setup_requires=['setuptools_scm'],
+    install_requires=["awscli",
+                      "botocore",
+                      "boto3",
+                      "gitpython",
+                      "jsonschema",
+                      "git+https://github.com/spulec/moto",
+                      "pandas",
+                      "pareto",
+                      "requests",
+                      "ruamel.yaml"]
+    include_package_data=True
+
 )
 
