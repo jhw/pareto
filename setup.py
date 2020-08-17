0dfb3e27e5807679ea6364f91c849f65201a700c
Justin <justin.worrall@gmail.com>
2020-08-17 08:17:09 +0100

    bumped version to 1.0.6

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def filter_pip_dependencies(root="requirements.txt"):
              
 setuptools.setup(
     name="pareto",
-    version="1.0.4",
+    version="1.0.6",
     author="jhw",
     author_email="justin.worrall@gmail.com",
     description="OTP for serverless",
